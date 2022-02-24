49a1ea84c551b8521349ac3d24974dc98d46d8eb
yuuunme <wjddbsk333@naver.com>
2022-02-24 11:13:23 +0900

    두번째 커밋

--- start/src/day03/Test01.java
+++ start/src/day03/Test01.java
@@ -3,5 +3,8 @@ package day03;
 public class Test01 {
 	public static void main(String[] args) {
 		System.out.println("*** Git 연동 성공 ***");
+		
+		System.out.println("깃 다시 성공");
+		System.out.println("깃 다시 성공");
 	}
 }
